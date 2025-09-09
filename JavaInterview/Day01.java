2b6a36b69bd73dedd99684b5b0b37b0aa4f9f82f
Kumar Mayank <kumarmayank2090@gmail.com>
2025-09-09 09:10:56 +0000

    inteview1

---
+++ JavaInterview/Day01.java
@@ -0,0 +1,45 @@
+//(1)W.A.P to check odd or even 
+//W.A.P to reverse the string
+// W.A.P to find factorial of any number
+// W.A.p to reverse of number
+//W.A.P to check the palindrome number
+//W.A.P to swap of two number without third variables
+//W.A.P to find lagest element in an array
+//W.A.P to fabonacci series up to n term 
+// W.A.P to check Prime number
+//Find the second largest number in an array
+//Count vowels and consonants in a string 
+
+//(1)
+/*import java .util.Scanner;
+class Oddeven
+{
+    public static void main(String[] args) {
+        System.out.println("Enter any number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%2==0)
+        {  System.out.println("It is even number");
+        }
+
+            else{
+      System.out.println("Enter any number:");
+            }
+    }
+}
+*/
+
+//(2)
+/*
+class StringReverse
+{
+    public static void main(String[] args) {
+        String str="kumar";
+        String rev="";
+        
+        if(int i=str.stringLength()-1; i>=0;i--){
+            rev=rev+String
+        }
+    }
+}
+*/
